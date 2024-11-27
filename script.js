7ccfc34ae499f9b4bde287523c86c24206a64e54
Ramdonx <40654074+Ramdonx@users.noreply.github.com>
2024-11-27 01:15:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,39 +2,26 @@ let currentPage = 0;
 
 const pages = document.querySelectorAll('.page');
 
-
 function showPage(index) {
-
     pages.forEach((page, i) => {
-
         page.style.transform = `translateX(${(i - index) * 100}%)`;
-
     });
-
 }
 
 
 document.getElementById('next').addEventListener('click', () => {
-
     if (currentPage < pages.length - 1) {
-
         currentPage++;
-
         showPage(currentPage);
-
     }
-
 });
-
 
 document.getElementById('prev').addEventListener('click', () => {
-
     if (currentPage > 0) {
-
         currentPage--;
-
         showPage(currentPage);
-
     }
-
 });
+
+// Inicializa la primera página
+showPage(currentPage);
